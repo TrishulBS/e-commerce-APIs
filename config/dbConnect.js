9d439616ae9d27fe28ec423dd41c584cbafa7291
TrishulBS <trishulcurtis@gmail.com>
2023-06-17 17:05:36 -0700

    db connection

---
+++ config/dbConnect.js
@@ -0,0 +1,12 @@
+const {default: mongoose} = require("mongoose")
+
+const dbConnect = () => {
+    try{
+        const conn = mongoose.connect("mongodb://127.0.0.1:27017/latestdb")
+        console.log("Database connected successfully")
+    } catch(error) {
+        console.log('Database error')
+    }
+}
+
+module.exports = dbConnect
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const dotenv = require('dotenv').config()
 const PORT = process.env.PORT || 4000
-
+const dbConnect = require("./config/dbConnect")
+dbConnect()
 app.use("/", (req, res)=>{
     res.send("Hello from server side")
 })
